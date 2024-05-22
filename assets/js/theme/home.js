f4d831075888bbb0d54e9d0fad361cfa3388be71
Le Nguyen Quang Minh <quangminh2170@gmail.com>
2024-05-22 16:35:39 +0700

    Done Home Gallery Section

--- assets/js/theme/home.js
+++ assets/js/theme/home.js
@@ -19,6 +19,10 @@ export default class Home extends PageManager {
             if(themeSettings.enable_header_layout_2 && window.innerWidth > 1024) {
                 this.hoverHomeMenu();
             }
+
+            if(window.innerWidth > 1024) {
+                this.hoverGallerySection();
+            }
         }
     }
 
@@ -29,6 +33,8 @@ export default class Home extends PageManager {
                 this.loadFunction();
             });
         });
+
+        this.loadScrollFunction();
     }
 
     hoverHomeMenu() {
@@ -46,5 +52,94 @@ export default class Home extends PageManager {
                 header.classList.remove('is-visible');
             });
         });
+    }
+
+    hoverGallerySection() {
+        const galleryTitles = document.querySelectorAll(".homeHoverBanner__title");
+
+        if (!galleryTitles) return;
+
+        galleryTitles.forEach((title) => {
+            title.addEventListener('mouseover', (e) => {
+                const parentTitle = title.closest('.homeHoverBanner__item');
+                const currentVisible = document.querySelector('.homeHoverBanner__item.is-visible');
+
+                if (!parentTitle || !currentVisible) return;
+
+                currentVisible.classList.remove('is-visible');
+                parentTitle.classList.add('is-visible');
+
+                const titleDataText = title.textContent;
+
+                if (currentVisible.querySelector('.homeHoverBanner__title').textContent === titleDataText) return;
+
+                changeBannerImage(titleDataText);
+            });
+        });
+
+        function changeBannerImage(dataName) {
+            const bannerImageList = document.querySelectorAll('.homeHoverBanner__imageItem');
+
+            if (!bannerImageList) return;
+
+            bannerImageList.forEach((image) => {
+                if (image.getAttribute('data-section-name') === dataName) {
+                    image.classList.add('is-visible');
+                } else {
+                    image.classList.remove('is-visible');
+                }
+            });
+        }
+    }
+
+    loadScrollFunction() {
+        const handler = (entries, observer) => {
+            entries.forEach(entry => {
+                if(entry.isIntersecting) {
+                    const section = entry.target;
+                    const sectionType = section.getAttribute('data-section-load');
+
+                    switch(sectionType) {
+                        case 'active-title':
+                            this.changeGalleryTitle(section);
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+            });
+        }
+
+        const sections = document.querySelectorAll('[data-section-load]'),
+        options = {
+            threshold: 1.0
+        };
+
+        if(!sections) return;
+
+        const observer = new IntersectionObserver(handler, options);
+
+        sections.forEach(section => {
+            observer.observe(section);
+        });
+    }
+
+    changeGalleryTitle(item) {
+        const name = item.getAttribute('data-section-name');
+
+        if (!name) return;
+
+        const currentVisible = document.querySelector(`.homeHoverBanner__item[data-title-name='${name}']`);
+
+        if (!currentVisible) return;
+
+        const currentItem = document.querySelector('.homeHoverBanner__item.is-visible');
+
+        if (currentItem) {
+            currentItem.classList.remove('is-visible');
+        }
+
+        currentVisible.classList.add('is-visible');
     }
 }
